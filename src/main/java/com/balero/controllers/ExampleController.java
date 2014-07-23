593b346654340d57818fd97642a7733cf2fb4423
Anibal Gomez <anibalgomez@icloud.com>
2014-07-23 17:19:15 -0500

    Multi-Lang Unit Test

---
+++ src/main/java/com/balero/controllers/ExampleController.java
@@ -0,0 +1,25 @@
+package com.balero.controllers;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+* i18n & L10n Unit test class
+*/
+public class ExampleController extends AbstractController{
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+                                                 HttpServletResponse response) throws Exception {
+
+        String message = "Hola prueba";
+        ModelAndView model = new ModelAndView("WelcomePage");
+        model.addObject("message", message);
+        return model;
+    }
+
+}
+
