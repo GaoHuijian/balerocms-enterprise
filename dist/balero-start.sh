af5cb0a8b2fc264aae72bc20d59747c0c789ad35
Anibal Gomez <anibalgomez@icloud.com>
2014-08-01 13:44:45 -0500

    Multi-website

--- dist/balero-start.sh
+++ dist/balero-start.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
+# Support Multi-Websites
 
-echo "Starting Balero CMS Enterprise Service..."
-nohup nice java -jar standalone.jar 2>&1
+# If port 8080 are in use change this
+port=8080
+# Print
+echo "Starting Balero CMS Enterprise Service... on http://localhost:$port/"
+# Run in background
+nohup nice java -jar standalone.jar -httpPort=$port 2>&1
--- dist/balero-stop.sh
+++ dist/balero-stop.sh
@@ -2,5 +2,4 @@
 # Grabs and kill a process from the pidlist that has the word myapp
 
 kill `ps -ef | grep standalone.jar | grep -v grep | awk '{ print $2 }'`
-
 echo "Killing Balero CMS Enterprise :-("
