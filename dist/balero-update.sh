ba9afb4e9af744dcd0fc393aaa4f031675c7a2be
Anibal Gomez <anibalgomez@icloud.com>
2014-08-03 12:49:38 -0500

    Bsh update script fix

--- dist/balero-update.sh
+++ dist/balero-update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Update Balero CMS .jar
+# Balero CMS Enterprise
+# Bash Update Balero CMS .jar
 
 #--------------------------------------------
 # the .jar file is simply downloaded to the .folder
@@ -8,7 +9,7 @@ echo "Updating..."
 echo "Killing Balero CMS Enterprise Service..."
 kill `ps -ef | grep standalone.jar | grep -v grep | awk '{ print $2 }'`
 rm -rf .extract
-rm rf standalone.jar
+rm -rf standalone.jar
 echo -ne Downloading Balero CMS Latest Version standalone.jar
 dots
 wget -q http://www.balerocms.com/dist/updates/latest/standalone.jar
